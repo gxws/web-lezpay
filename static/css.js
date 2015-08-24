0e834b2f09d4ddee35cb646b9105d5161b07b76b
jqjjian-pc <jqjjian@126.com>
2015-08-24 16:08:11 +0800

    20150824

--- static/css.js
+++ static/css.js
@@ -1,10 +1,15 @@
 return {
 	files: {
 		/* begin 基础模块 */
-		'{static}/web-DBCL/static/css/base.mod.css': [
-			'{static}/web-DBCL/static/css/base.min.css',
-			'{static}/web-DBCL/static/css/base_HD.min.css'
-		]
+		'{static}/web-lezpay/css/_base.mod.css': [
+			'{static}/public/css/_public.min.css',
+			'{static}/web-lezpay/css/_base.min.css',
+		],
+		//0002&0003机顶盒
+		'{static}/web-lezpay/css/base.mod.css': [
+			'{static}/public/css/public.min.css',
+			'{static}/web-lezpay/css/base.min.css',
+		],
 		/* end 基础模块 */
 	}
 }
